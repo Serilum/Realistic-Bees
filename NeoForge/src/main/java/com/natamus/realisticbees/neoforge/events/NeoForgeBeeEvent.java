7fafcce732bd6f552361e6a32104c2fd60cd9101
Rick South <rick@natamus.com>
2024-05-26 14:56:58 +0200

    (v3.9) (1.20.6 Neo) Updated mod to work with NeoForge version 20.6.78+.

--- NeoForge/src/main/java/com/natamus/realisticbees/neoforge/events/NeoForgeBeeEvent.java
+++ NeoForge/src/main/java/com/natamus/realisticbees/neoforge/events/NeoForgeBeeEvent.java
@@ -3,15 +3,15 @@ package com.natamus.realisticbees.neoforge.events;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.realisticbees.events.BeeEvent;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
-import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 
 @EventBusSubscriber
 public class NeoForgeBeeEvent {
 	@SubscribeEvent
-	public static void onBeeCheckSpawn(MobSpawnEvent.FinalizeSpawn e) {
+	public static void onBeeCheckSpawn(MobSpawnEvent.PositionCheck e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
