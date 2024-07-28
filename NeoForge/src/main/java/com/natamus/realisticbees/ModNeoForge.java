e9fcff9bcbba00a07812ff6b08714d1b8a2a7645
Rick South <rick@natamus.com>
2024-07-28 07:17:48 +0200

    (v4.0) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/realisticbees/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/realisticbees/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.realisticbees;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.realisticbees.neoforge.events.NeoForgeBeeEvent;
 import com.natamus.realisticbees.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.realisticbees.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
