eb29c4435132e8298f372e0d44b7863b5d500a07
Rick South <rick@natamus.com>
2024-08-09 08:08:42 +0200

    Realistic Bees source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/realisticbees/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.realisticbees;
+
+import com.natamus.realisticbees.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ Common/src/main/java/com/natamus/realisticbees/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,22 @@
+package com.natamus.realisticbees.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.natamus.realisticbees.config.ConfigHandler;
+import net.minecraft.client.renderer.entity.BeeRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = LivingEntityRenderer.class, priority = 1001)
+public abstract class LivingEntityRendererMixin {
+	@Inject(method = "scale", at = @At("HEAD"))
+	private void applyScale(LivingEntity entity, PoseStack stack, float partialTicks, CallbackInfo ci) {
+		if (((Object) this) instanceof BeeRenderer) {
+			float scale = (float)ConfigHandler.beeSizeModifier;
+			stack.scale(scale, scale, scale);
+		}
+	}
+}
