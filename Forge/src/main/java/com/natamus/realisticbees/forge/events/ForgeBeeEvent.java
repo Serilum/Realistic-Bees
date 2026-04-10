0db8a350d5d6cc280c05ba66d13d2ebf802225cb
Rick South <rick@natamus.com>
2026-04-10 02:45:59 +0200

    Realistic Bees source code for 26.1.2.

---
+++ Forge/src/main/java/com/natamus/realisticbees/forge/events/ForgeBeeEvent.java
@@ -0,0 +1,32 @@
+package com.natamus.realisticbees.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.realisticbees.events.BeeEvent;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeBeeEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeBeeEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onBeeCheckSpawn(MobSpawnEvent.FinalizeSpawn e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		BeeEvent.onBeeCheckSpawn(e.getEntity(), level, null, null, e.getSpawnReason());
+	}
+
+	@SubscribeEvent
+	public static void onBeeSpawn(EntityJoinLevelEvent e) {
+		BeeEvent.onBeeSpawn(e.getEntity(), e.getLevel());
+	}
+}
